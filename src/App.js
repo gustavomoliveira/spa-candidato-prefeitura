99c2ba030d8d95bb502679cf88f8e4bfc11d078d
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2025-12-08 17:00:57 -0300

    feat: adiciona componente Agenda com 6 eventos e layout responsivo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles/global.css";
 import Header from './components/Header';
 import Biografia from "./components/Biografia";
 import Proposta from "./components/Proposta";
+import Agenda from "./components/Agenda";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           <Header />
           <Biografia />
           <Proposta />
+          <Agenda />
       </div>
   );
 }
